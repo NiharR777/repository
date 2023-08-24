cda17a77e3184e9d3473c9f984df03b8e1135e8f
NiharR777 <niharrniharr123@gmail.com>
2023-08-24 16:03:43 +0530

    adding_element_to_list

--- list.py
+++ list.py
@@ -1,6 +1,4 @@
-names =  ['N','I','H','A','R','.','R','G','O','W','D','A','7']
-print(names[2:6])
-print(names[5:])
-print(names[3:7])
-print(names[1:4])
-print(names[:])
+numbers =  [21, 34, 54, 12]
+print("before append:", numbers)
+numbers.append(77)
+print("after append:", numbers)
