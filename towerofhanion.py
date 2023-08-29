1fa6b8a45eb50ca2e484594f21803d433aa2aa4e
NiharR777 <niharrniharr123@gmail.com>
2023-08-29 10:58:18 +0530

    adding a file

---
+++ towerofhanion.py
@@ -0,0 +1,9 @@
+def TowerOfHanoi(n,source,destination,auxiliary):
+    if n==1:
+        print("move disk 1 form source",source,"to destination",destination)
+        return
+    TowerOfHanoi(n-1,source,auxiliary,destination)
+    print("move disk",n,"from source",source,"to destination",destination)
+    TowerOfHanoi(n-1,auxiliary,destination,source)
+n = 4
+TowerOfHanoi(n,'A','B','C')
